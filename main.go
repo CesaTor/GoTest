a6b5285a41a46856056c0b0bb278f1c669f42d5f
Cesare Torchia <cesare.torchia.98@gmail.com>
2018-01-18 22:03:39 +0100

    init

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"html/template"
+	"os"
+)
 
-func main()  {
-	fmt.Print("Hello World")
+func RootHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "HOME")
 }
 
+func MemeHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
 
+	tpl, err := template.ParseGlob("assets/templates/*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tpl.ExecuteTemplate(res, "meme.gohtml", nil)
+	fmt.Fprintln(res, "<h1>This is my meme</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", MemeHandler)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/img/", http.StripPrefix("/img", http.FileServer(http.Dir("./assets/img"))))
+
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
